55162a5093e5785dae11e8970ee7f4d7c96af5fa
jonas <>
2023-04-21 19:45:47 +0200

    shadows

--- three_js_tests/main.js
+++ three_js_tests/main.js
@@ -2,11 +2,16 @@
 import * as THREE from 'three'
 import Stats from 'stats.js'
 
+// create and add renderer to document
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true; // enables shadows
+document.body.appendChild(renderer.domElement);
+
 // fps counter using stats
 const stats = new Stats()
 stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom)
-
 
 // instantiate scene
 const scene = new THREE.Scene();
@@ -17,33 +22,34 @@ const aspect_ratio = window.innerWidth / window.innerHeight;
 const near = 0.1;
 const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect_ratio, near, far);
-
-// create and add renderer to document
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// helper (debug) camera
+const camera_helper = new THREE.CameraHelper(camera);
+scene.add(camera_helper);
 
 // create cube
 const cube_geometry = new THREE.BoxGeometry(1, 1, 1);
-const cube_material = new THREE.MeshStandardMaterial({ color: 0x2ff22 }); // greenish
+const cube_material = new THREE.MeshStandardMaterial({ color: 0x22ff22 }); // greenish
 const cube = new THREE.Mesh(cube_geometry, cube_material);
+cube.castShadow = true;
 scene.add(cube);
 
 // create ground plane
-const plane_geometry = new THREE.PlaneGeometry(10, 10);
-const plane_material = new THREE.MeshBasicMaterial({ color: 0x2222ff, side: THREE.DoubleSide });
+const plane_geometry = new THREE.PlaneGeometry(100, 100);
+const plane_material = new THREE.MeshStandardMaterial({ color: 0x2222ff, side: THREE.DoubleSide });
 const plane = new THREE.Mesh(plane_geometry, plane_material);
 plane.rotation.x = Math.PI / 2;
-plane.position.set(0, -5, -10);
+plane.position.set(0, -1, 0);
+plane.receiveShadow = true;
 scene.add(plane);
 
 // add point light
-const point_light = new THREE.PointLight(0xff2222, 10, 100); // redish
-point_light.position.set(10, 10, 10);
+const point_light = new THREE.PointLight(0xffffff, 1, 100); // redish
+point_light.position.set(3, 10, 5);
+point_light.castShadow = true;
 scene.add(point_light);
 
 // add ambient light
-const ambient_light = new THREE.AmbientLight(0xff2222, 1, 100); // redish
+const ambient_light = new THREE.AmbientLight(0xffffff, 1/10, 100); // redish
 scene.add(ambient_light);
 
 // move camera because else the camera is inside the cube
