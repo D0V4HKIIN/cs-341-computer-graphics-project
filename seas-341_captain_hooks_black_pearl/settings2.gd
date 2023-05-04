b0574dd1fa8965b65da5169d752588ee1dd71319
nepheleaes <s2056170@ed.ac.uk>
2023-05-04 17:05:37 +0200

    DiscreteFourierTransform start shader

--- seas-341_captain_hooks_black_pearl/settings2.gd
+++ seas-341_captain_hooks_black_pearl/settings2.gd
@@ -1,9 +1,5 @@
 extends Control
 
-# settings
-@onready var SPEED = $MarginContainer/VBoxContainer/HSlider
-@onready var tickbox = $MarginContainer/VBoxContainer/CheckBox
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
