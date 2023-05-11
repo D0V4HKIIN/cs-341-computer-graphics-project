aaf763b040f90adda3d5732803d0fc79edbdb179
jonas <>
2023-05-11 16:42:09 +0200

    radio buttons ui

--- seas-341_captain_hooks_black_pearl/settings2.gd
+++ seas-341_captain_hooks_black_pearl/settings2.gd
@@ -2,15 +2,6 @@ extends Control
 
 @onready var ocean = $"../Ocean"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_h_slider_value_changed(value):
 	for water in get_tree().get_nodes_in_group("Water"):
 		water.get_active_material(0).set("shader_parameter/height_scale", value);
