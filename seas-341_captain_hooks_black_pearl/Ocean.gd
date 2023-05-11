b13790a22154c69527cf50d5e942a02bdc4eacf2
jonas <>
2023-05-11 15:17:04 +0200

    rest boat + moved code to ocean + noise_frequency

--- seas-341_captain_hooks_black_pearl/Ocean.gd
+++ seas-341_captain_hooks_black_pearl/Ocean.gd
@@ -12,7 +12,6 @@ var rng = RandomNumberGenerator.new();
 var random_number_xi;
 
 
-
 # creates a bunch of water tiles and sets the position, subdivision and scale
 func createOcean():
 	for i in spawnPoint.spawnPoints.size():
@@ -34,8 +33,68 @@ func createOcean():
 		waterInstance.set_scale(Vector3(tileScale, 1.0, tileScale)); # Ignore Y value because planes are 2d
 		
 		waterInstance.add_to_group("Water"); # to get the water meshes in the ui
+
+# Water shader varialbes
+var water_noise : NoiseTexture2D;
+var noise : Image
+var height_scale = 1;
+var noise_frequency = 1;
+
+func update_variables():
+	print("updating variables");
+	var tree = get_tree();
+	var material = tree.get_first_node_in_group("Water").get_active_material(0);
+	
+	# noise_frequency
+	noise_frequency = material.get("shader_parameter/noise_frequency");
+	
+	# height_scale
+	height_scale = material.get("shader_parameter/height_scale");
+	
+	# noise texture
+	water_noise = material.get("shader_parameter/noise");
+	await water_noise.changed
+	noise = water_noise.get_image();
+
 # called when scene is instanciated
 func _ready():
 	seed(1);
 	random_number_xi = rng.randfn();
-	#createOcean();
+	
+	createOcean();
+	
+	update_variables();
+
+var cpu_time = 0.0;
+func _process(delta):
+	# compute time so that it's the same in cpu and gpu
+	cpu_time += delta
+	
+	# set cpu_time
+	var tree = get_tree();
+	for water in tree.get_nodes_in_group("Water"):
+		water.get_active_material(0).set("shader_parameter/cpu_time", cpu_time);
+
+func wave(pos):
+	var uv_x = wrapf(pos.x * noise_frequency, 0, 1)
+	var uv_y = wrapf(pos.y * noise_frequency, 0, 1)
+	var pixel_pos = Vector2(uv_x * noise.get_width(), uv_y * noise.get_height())
+
+	pos += Vector2.ONE * (noise.get_pixelv(pixel_pos).r * 2.0 - 1.0);
+	
+	var wv = Vector2.ONE - Vector2(abs(sin(pos.x)), abs(sin(pos.y)));
+	return pow(1.0 - pow(wv.x * wv.y, 0.65), 4.0);
+
+
+func noise_height(pos, time):
+	var d = wave((pos + Vector2.ONE * time) * 0.4) * 0.3;
+	d += wave((pos - Vector2.ONE * time) * 0.3) * 0.3;
+	d += wave((pos + Vector2.ONE * time) * 0.5) * 0.2;
+	d += wave((pos - Vector2.ONE * time) * 0.6) * 0.2;
+	return d * height_scale;
+
+
+func get_water_height(pos):
+	if(noise == null):
+		return 0;
+	return noise_height(pos, cpu_time);
--- seas-341_captain_hooks_black_pearl/settings2.gd
+++ seas-341_captain_hooks_black_pearl/settings2.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var ocean = $"../Ocean"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace wo0pith function body.
@@ -12,6 +14,7 @@ func _process(delta):
 func _on_h_slider_value_changed(value):
 	for water in get_tree().get_nodes_in_group("Water"):
 		water.get_active_material(0).set("shader_parameter/height_scale", value);
+	ocean.update_variables();
 
 
 func _on_check_box_toggled(button_pressed):
