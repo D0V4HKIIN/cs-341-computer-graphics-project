220bb73aec70318b27283f4ebd2e8a39ee46a96f
jonas <>
2023-05-29 17:19:44 +0200

    water physics

--- seas-341_captain_hooks_black_pearl/Ocean.gd
+++ seas-341_captain_hooks_black_pearl/Ocean.gd
@@ -43,7 +43,9 @@ func createOcean():
 
 # Water shader varialbes
 var water_noise : NoiseTexture2D;
+var water_noise2 : NoiseTexture2D;
 var noise : Image
+var noise2 : Image
 var height_scale = 1;
 var wave_speed = 1;
 var noise_frequency = 1;
@@ -64,8 +66,11 @@ func update_variables():
 	
 	# noise texture
 	water_noise = material.get("shader_parameter/noise");
+	water_noise2 = material.get("shader_parameter/noise2");
 	await water_noise.changed
+	await water_noise2.changed
 	noise = water_noise.get_image();
+	noise2 = water_noise2.get_image();
 
 # called when scene is instanciated
 func _ready():
@@ -86,22 +91,24 @@ func _process(delta):
 	for water in tree.get_nodes_in_group("Water"):
 		water.get_active_material(0).set("shader_parameter/cpu_time", cpu_time);
 
-func wave(pos):
-	var uv_x = wrapf(pos.x * noise_frequency, 0, 1)
-	var uv_y = wrapf(pos.y * noise_frequency, 0, 1)
+func wave(time_pos, pos):
+	var uv_x = wrapf(time_pos.x * noise_frequency, 0, 1)
+	var uv_y = wrapf(time_pos.y * noise_frequency, 0, 1)
 	var pixel_pos = Vector2(uv_x * noise.get_width(), uv_y * noise.get_height())
 
-	pos += Vector2.ONE * (noise.get_pixelv(pixel_pos).r * 2.0 - 1.0);
+	time_pos += Vector2.ONE * (noise.get_pixelv(pixel_pos).r * 2.0 - 1.0);
 	
-	var wv = 1 - abs(sin(pos.x + pos.y));
-	return pow(1.0 - pow(wv, 0.65), 4.0);
+	var height_delta = noise2.get_pixelv(pos * noise_frequency).r * 0.2;
+	
+	var wv = 1 - abs(sin((time_pos.x + time_pos.y) * 0.8));
+	return pow(1.0 - pow(wv, 0.65), 4.0) + height_delta;
 
 
 func noise_height(pos, time):
-	var d = wave((pos + Vector2.ONE * time) * 0.4) * 0.3;
-	d += wave((pos + Vector2.ONE * time) * 0.3) * 0.3;
-	d += wave((pos + Vector2.ONE * time) * 0.5) * 0.2;
-	d += wave((pos + Vector2.ONE * time) * 0.6) * 0.2;
+	var d = wave((pos + Vector2.ONE * time) * 0.4, pos) * 0.3;
+	d += wave((pos + Vector2.ONE * time * 1.1) * 0.3, pos) * 0.3;
+	d += wave((pos + Vector2.ONE * time * 0.9) * 0.5, pos) * 0.2;
+	d += wave((pos + Vector2.ONE * time) * 0.6, pos) * 0.2;
 	return d * height_scale;
 
 
