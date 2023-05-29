14fd68c11800531b11ed761df5d2a6fad5ca8094
tonics <67322225+tonichencs@users.noreply.github.com>
2023-05-29 20:43:33 +0200

    Export boids variables so we can customize it, add more comments to sky shader and boids algorithm

--- seas-341_captain_hooks_black_pearl/Birds/manyBirds.gd
+++ seas-341_captain_hooks_black_pearl/Birds/manyBirds.gd
@@ -3,51 +3,52 @@ extends MultiMeshInstance3D
 
 @onready var box = $"../birds/Box"
 
-var box_vertices: Vector3
+var boxVerification: Vector3
 
-var BOID_COUNT = 10
-const BOID_SPEED_MIN = 3.0
-const BOID_SPEED_MAX = 10.0
-const NEIGHBOR_DISTANCE = 3.0
-const AVOIDANCE_DISTANCE = 2.0
-const ALIGNMENT_WEIGHT = 0.6
-const COHESION_WEIGHT = 0.01
-const SEPARATION_WEIGHT = 0.7
-const BOUNDARY_DISTANCE = 7.0
-const BOUNDARY_WEIGHT = 0.3
-const ATTRACTION_WEIGHT = 0.1
+@export var boidCount = 10
+@export var boidSpeedMin= 3.0
+@export var boidSpeedMax = 10.0
+@export var neighbourDistance = 3.0
+@export var avoidanceDistance = 2.0
+@export var alignmentWeight = 0.6
+@export var coheshionWeight = 0.01
+@export var separationWeight = 0.7
+@export var boundaryDistance = 7.0
+@export var boundaryForceWeight = 0.3
+@export var attactionWeight = 0.1
 
-var attractor_positions: PackedVector3Array
-@export var attractor_count = 100
+var attractorPositions: PackedVector3Array
+@export var attractorCount = 100
 
-var boid_positions: PackedVector3Array
-var boid_velocities: PackedVector3Array
-var min_vertex = Vector3(INF, INF, INF)
-var max_vertex = Vector3(-INF, -INF, -INF)
+var boidPositions: PackedVector3Array
+var boidVelocities: PackedVector3Array
+var minVertex = Vector3(INF, INF, INF)
+var maxVertex = Vector3(-INF, -INF, -INF)
 
 func setup():
-	BOID_COUNT = multimesh.instance_count
-	boid_positions.resize(BOID_COUNT)
-	boid_velocities.resize(BOID_COUNT)
-	attractor_positions.resize(attractor_count)
+	boidCount = multimesh.instance_count
+	boidPositions.resize(boidCount)
+	boidVelocities.resize(boidCount)
+	attractorPositions.resize(attractorCount)
 	
+	# Get the aabb of the bounding box and set the min and max coordinates birds can go
 	var box_aabb = box.get_aabb()
 	
 	for i in range(8):
 		var curr = box_aabb.get_endpoint(i)
-		if min_vertex > curr:
-			min_vertex = curr
+		if minVertex > curr:
+			minVertex = curr
 		
-		if max_vertex < curr:
-			max_vertex = curr
+		if maxVertex < curr:
+			maxVertex = curr
 			
-	for i in range(attractor_count):
-		attractor_positions[i] = Vector3(randf_range(min_vertex.x, max_vertex.x), randf_range(min_vertex.y, max_vertex.y), randf_range(min_vertex.z, max_vertex.z))
+	for i in range(attractorCount):
+		attractorPositions[i] = Vector3(randf_range(minVertex.x, maxVertex.x), randf_range(minVertex.y, maxVertex.y), randf_range(minVertex.z, maxVertex.z))
 	
-	for i in range(BOID_COUNT):
-		boid_positions[i] = Vector3(randf_range(min_vertex.x, max_vertex.x), randf_range(min_vertex.y, max_vertex.y), randf_range(min_vertex.z, max_vertex.z))
-		boid_velocities[i] = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1)).normalized() * randf_range(BOID_SPEED_MIN, BOID_SPEED_MAX)
-		multimesh.set_instance_transform(i, Transform3D(Basis(), boid_positions[i]))
+	for i in range(boidCount):
+		boidPositions[i] = Vector3(randf_range(minVertex.x, maxVertex.x), randf_range(minVertex.y, maxVertex.y), randf_range(minVertex.z, maxVertex.z))
+		boidVelocities[i] = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1)).normalized() * randf_range(boidSpeedMin, boidSpeedMax)
+		multimesh.set_instance_transform(i, Transform3D(Basis(), boidPositions[i]))
 		multimesh.set_instance_custom_data(i, Color(randf(), randf(), randf(), randf()))
 
 
@@ -58,93 +59,91 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	for i in range(multimesh.instance_count):
-		var position = boid_positions[i]
-		var velocity = boid_velocities[i]
+		var position = boidPositions[i]
+		var velocity = boidVelocities[i]
 
 		# Get the average velocity and position of neighboring boids
-		var average_velocity = Vector3()
-		var average_position = Vector3()
+		var averageVelocity = Vector3()
+		var averagePosition = Vector3()
+		
 		var separation = Vector3()
-		var neighbor_count = 0
+		var neighborCount = 0
 
 		for j in range(multimesh.instance_count):
 			if i != j:
-				var neighbor_position = boid_positions[j]
+				var neighborPosition = boidPositions[j]
 
 				# Calculate distance to neighbor
-				var distance = position.distance_to(neighbor_position)
+				var distance = position.distance_to(neighborPosition)
 
-				# Check if neighbor is within the neighbor distance
-				if distance < NEIGHBOR_DISTANCE:
-					average_velocity += boid_velocities[j]
-					average_position += neighbor_position
+				# Check if the birds is within the acceptable neighbour distance
+				if distance < neighbourDistance:
+					averageVelocity += boidVelocities[j]
+					averagePosition += neighborPosition
 
-				# Separation behavior
-				if distance < AVOIDANCE_DISTANCE:
-					separation += (neighbor_position - position).normalized() / distance
+				# Separate birds that are too close to its neighbours
+				if distance < avoidanceDistance:
+					separation += (neighborPosition - position).normalized() / distance
 
-				neighbor_count += 1
+				neighborCount += 1
 
-		if neighbor_count > 0:
-			average_velocity /= neighbor_count
-			average_position /= neighbor_count
-			separation /= neighbor_count
+		if neighborCount > 0:
+			averageVelocity /= neighborCount
+			averagePosition /= neighborCount
+			separation /= neighborCount
 
-			var alignment = average_velocity.normalized()
-			var cohesion = (average_position - position).normalized()
+			var alignment = averageVelocity.normalized()
+			var cohesion = (averagePosition - position).normalized()
 
-			var steer = alignment * ALIGNMENT_WEIGHT + cohesion * COHESION_WEIGHT - separation * SEPARATION_WEIGHT
+			var steer = alignment * alignmentWeight + cohesion * coheshionWeight - separation * separationWeight
 			
-			# Apply boundary avoidance behavior
-			var boundary_force = calculate_boundary_force(position)
-			steer += boundary_force * BOUNDARY_WEIGHT
+			# Birds should smoothly turn away from the boundary of the bounding mesh
+			var boundaryForce = calculate_boundary_force(position)
+			steer += boundaryForce * boundaryForceWeight
 			
-			# Attraction towards attractor positions
-			var attractor_force = calculate_attractor_force(position)
-			steer += attractor_force * ATTRACTION_WEIGHT
+			# Added attraction force to attracting points so that boids can be separated by these points
+			var attractorForce = calculate_attractor_force(position)
+			steer += attractorForce * attactionWeight
 			
-			# Apply steering force to boid
-			velocity = (velocity + steer).normalized() * randf_range(BOID_SPEED_MIN, BOID_SPEED_MAX)
+			# Generate new speed in a range so velocit isn't always the same.
+			velocity = (velocity + steer).normalized() * randf_range(boidSpeedMin, boidSpeedMax)
 
 			# Apply steering force to boid
 			position += velocity * delta
 
-			# Perform boundary checks
-			position = clamp(position, min_vertex,  max_vertex)
-
-			boid_positions[i] = position
-			boid_velocities[i] = velocity
+			boidPositions[i] = position
+			boidVelocities[i] = velocity
 			var rotation = Transform3D().looking_at(-velocity, Vector3.UP)
 			
 			multimesh.set_instance_transform(i, Transform3D(rotation.basis, position))
 
 func calculate_boundary_force(position):
-	var boundary_force = Vector3.ZERO
+	var boundaryForce = Vector3.ZERO
 
-	if position.x < min_vertex.x + BOUNDARY_DISTANCE:
-		boundary_force.x = 1.0 - (position.x - min_vertex.x) / BOUNDARY_DISTANCE
-	elif position.x > max_vertex.x - BOUNDARY_DISTANCE:
-		boundary_force.x = -1.0 + (position.x - max_vertex.x) / BOUNDARY_DISTANCE
+	if position.x < minVertex.x + boundaryDistance:
+		boundaryForce.x = 1.0 - (position.x - minVertex.x) / boundaryDistance
+	elif position.x > maxVertex.x - boundaryDistance:
+		boundaryForce.x = -1.0 + (position.x - maxVertex.x) / boundaryDistance
 
-	if position.y < min_vertex.y + BOUNDARY_DISTANCE:
-		boundary_force.y = 1.0 - (position.y - min_vertex.y) / BOUNDARY_DISTANCE
-	elif position.y > max_vertex.y - BOUNDARY_DISTANCE:
-		boundary_force.y = -1.0 + (position.y - max_vertex.y) / BOUNDARY_DISTANCE
+	if position.y < minVertex.y + boundaryDistance:
+		boundaryForce.y = 1.0 - (position.y - minVertex.y) / boundaryDistance
+	elif position.y > maxVertex.y - boundaryDistance:
+		boundaryForce.y = -1.0 + (position.y - maxVertex.y) / boundaryDistance
 
-	if position.z < min_vertex.z + BOUNDARY_DISTANCE:
-		boundary_force.z = 1.0 - (position.z - min_vertex.z) / BOUNDARY_DISTANCE
-	elif position.z > max_vertex.z - BOUNDARY_DISTANCE:
-		boundary_force.z = -1.0 + (position.z - max_vertex.z) / BOUNDARY_DISTANCE
+	if position.z < minVertex.z + boundaryDistance:
+		boundaryForce.z = 1.0 - (position.z - minVertex.z) / boundaryDistance
+	elif position.z > maxVertex.z - boundaryDistance:
+		boundaryForce.z = -1.0 + (position.z - maxVertex.z) / boundaryDistance
 
-	return boundary_force
+	return boundaryForce
 
 func calculate_attractor_force(position):
-	var attractor_force = Vector3.ZERO
+	var attractorForce = Vector3.ZERO
 
-	for attractor_pos in attractor_positions:
-		attractor_force += (attractor_pos - position).normalized()
+	for attractorPos in attractorPositions:
+		attractorForce += (attractorPos - position).normalized()
 
-	if attractor_positions.size() > 0:
-		attractor_force /= attractor_positions.size()
+	if attractorPositions.size() > 0:
+		attractorForce /= attractorPositions.size()
 
-	return attractor_force
+	return attractorForce
